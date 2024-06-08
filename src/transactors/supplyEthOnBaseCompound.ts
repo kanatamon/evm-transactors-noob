7c5930e02936d8c851361956a17fe91035730a73
Kantapon Sritiyot <o_kantapon.s@kbtg.tech>
2024-06-08 14:25:30 +0700

    fix: gas limit calculation

--- src/transactors/supplyEthOnBaseCompound.ts
+++ src/transactors/supplyEthOnBaseCompound.ts
@@ -1,8 +1,15 @@
 import { ethers } from 'ethers';
 import cEthAbi from './supplyEthOnBaseCompound.abi';
-import { toDoubleNumber } from '../utils';
+import { availableRpc, toDoubleNumber } from '../utils';
 
-const RPC_URL = `https://mainnet.base.org`;
+const RPC_URLS = [
+  `https://mainnet.base.org`,
+  `https://base-rpc.publicnode.com`,
+  `https://base.meowrpc.com`,
+  `https://base-pokt.nodies.app`,
+  `https://base.drpc.org`,
+  `https://base.llamarpc.com`,
+];
 
 // https://docs.compound.finance/#protocol-contracts
 const CONTRACT_ADDRESS = '0x78D0677032A35c63D142a48A2037048871212a8C';
@@ -10,7 +17,8 @@ const CONTRACT_ADDRESS = '0x78D0677032A35c63D142a48A2037048871212a8C';
 const COMET_ADDRESS = '0x46e6b214b524310239732D51387075E0e70970bf';
 
 export async function supplyEthOnCompound(privateKey: string, amount: string) {
-  const provider = new ethers.JsonRpcProvider(RPC_URL);
+  // const provider = new ethers.JsonRpcProvider(RPC_URL);
+  const provider = await availableRpc(RPC_URLS);
   const wallet = new ethers.Wallet(privateKey, provider);
   const cEthContract = new ethers.Contract(CONTRACT_ADDRESS, cEthAbi, wallet);
 
@@ -22,7 +30,7 @@ export async function supplyEthOnCompound(privateKey: string, amount: string) {
   );
   const actionCalldata = await cEthContract.ACTION_SUPPLY_NATIVE_TOKEN();
 
-  const args = [actionCalldata, supplyAssetCalldata];
+  const args = [[actionCalldata], [supplyAssetCalldata]];
 
   // console.log(`Supplying ETH on Compound`);
   // console.log(`Address: ${wallet.address}`);
@@ -30,6 +38,8 @@ export async function supplyEthOnCompound(privateKey: string, amount: string) {
   const estimatedGas = await cEthContract.invoke.estimateGas(...args, {
     value: amountInWei,
   });
+  // console.log('Estimated gas:', estimatedGas.toString());
+  // console.log(`Double gas limit: ${toDoubleNumber(estimatedGas)}`);
   const tx = await cEthContract.invoke(...args, {
     value: amountInWei,
     gasLimit: toDoubleNumber(estimatedGas),
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,22 @@
 import { ethers } from 'ethers';
 
 export function toDoubleNumber(n: bigint) {
-  return Math.ceil(Number(ethers.formatEther(n)) * 2);
+  return n * BigInt(2);
+}
+
+// TODO: use better solution to check if the provider is working
+export async function availableRpc(urls: string[]) {
+  for (const url of urls) {
+    try {
+      const provider = new ethers.JsonRpcProvider(url);
+      await provider.getBlockNumber(); // Test if the provider is working
+      console.log(`Using provider: ${url}`);
+      return provider;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.warn(`Provider ${url} failed: ${error.message}`);
+      }
+    }
+  }
+  throw new Error('No working providers found');
 }
